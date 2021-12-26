8b60c0713c18e3177f5bb7c9fb87daa8d62d7b93
Mateo Ceballos Querol <mateo.cebquerol@gmail.com>
2021-12-26 16:55:37 +0000

    added myCode\CBalance.h

---
+++ myCode/CBalance.h
@@ -0,0 +1,12 @@
+#ifndef CBALANCE_H
+#define CBALANCE_H
+class CBalance {
+private:
+    unsigned int balance;
+public:
+
+    unsigned int getBalance();
+    void setBalance(unsigned int balance);
+};
+
+#endif /* CBALANCE_H */
