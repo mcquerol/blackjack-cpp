f7f7051f1d051a37d53106d0d7774903c917a640
Mateo Ceballos Querol <mateo.cebquerol@gmail.com>
2022-02-17 01:04:48 +0000

    added myCode\main.cpp

---
+++ myCode/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include "CGame.h"
+
+using namespace std;
+
+int main (void)
+{
+	CGame g;
+
+	g.start();
+
+	//TODO
+	/*
+	 * shuffle the array
+	 * game logic
+	 * 		players
+	 * 		balance per player
+	 * 		dealer
+	 */
+	return 0;
+}
